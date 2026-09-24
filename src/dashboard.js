afbc4460b47c7779308143a2b82e229fc0b32218
Belloni Alessandro <spupuz@gmail.com>
2026-09-24 11:02:01 +0200

    chore: release v1.36.0

--- src/dashboard.js
+++ src/dashboard.js
@@ -893,7 +893,7 @@ margin-top: 2px;
 			<div class="footer-copy">
 				&copy; 2026 Alessandro Belloni.<br>
 				<div style="margin-top: 0.5rem; font-size: 0.8rem; font-family: monospace; color: var(--text-muted);">
-					telemetry version <span id="telemetry-version">v1.37.25</span>
+					telemetry version <span id="telemetry-version">v1.36.0</span>
 				</div>
 			</div>
 		</div>
