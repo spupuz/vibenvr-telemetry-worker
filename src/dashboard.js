cfbaf2097b81c2fd7add3a1e14e416892760c6d1
Belloni Alessandro <spupuz@gmail.com>
2026-09-25 15:51:15 +0200

    v1.37.28: merge PRs and bump version

--- src/dashboard.js
+++ src/dashboard.js
@@ -902,7 +902,7 @@ margin-top: 2px;
 			<div class="footer-copy">
 				&copy; 2026 Alessandro Belloni.<br>
 				<div style="margin-top: 0.5rem; font-size: 0.8rem; font-family: monospace; color: var(--text-muted);">
-					telemetry version <span id="telemetry-version">v1.37.27</span>
+					telemetry version <span id="telemetry-version">v1.37.28</span>
 				</div>
 			</div>
 		</div>
